059be603fc8609c7c53926760f6c4b5d73b2f3c9
Luca Zoccheddu <luca17.zok@gmail.com>
2023-03-28 11:21:30 +0200

    aggiunta detail, prima dei children routing

---
+++ src/app/components/recipes/detail/detail.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Recipe } from 'src/app/models/recipe.model';
+import { RecipeService } from 'src/app/services/recipe.service';
+
+@Component({
+  selector: 'app-detail',
+  templateUrl: './detail.component.html',
+  styleUrls: ['./detail.component.scss']
+})
+export class DetailComponent implements OnInit{
+
+  ricetta: Recipe;
+
+  constructor(
+    private activetedRoute: ActivatedRoute,
+    private router: Router,
+    private recipeService: RecipeService
+    ) {};
+
+  ngOnInit(): void {
+    this.onGetRecipe();
+  }
+
+  onGetRecipe(): void {
+    const id = Number(this.activetedRoute.snapshot.paramMap.get('_id'));
+    this.recipeService.getRecipe(id).subscribe({
+      next: (res) => {
+        this.ricetta = res;
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    });
+  }
+
+  onGetRecipe2(): void {
+    this.activetedRoute.params.subscribe((parametriUrl) => {
+      const id = Number(parametriUrl['_id']);
+      this.recipeService.getRecipe(id).subscribe({
+        next: (res) => {
+          this.ricetta = res;
+        },
+        error: (err) => {
+          console.log(err);
+        }
+    })
+  })
+}
+}
